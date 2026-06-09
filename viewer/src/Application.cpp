d6fd281df5644b2a5afde2f3ca65d6760d138733
Evan <evanrock520l@ciencias.unam.mx>
2026-06-08 19:56:38 -0600

    fix: change includes order

--- viewer/src/Application.cpp
+++ viewer/src/Application.cpp
@@ -3,8 +3,8 @@
 
 #include "Application.hpp"
 
-#include <GLFW/glfw3.h>
 #include <glad/gl.h>
+#include <GLFW/glfw3.h>
 #include <imgui.h>
 #include <imgui_impl_glfw.h>
 #include <imgui_impl_opengl3.h>
