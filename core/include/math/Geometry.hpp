155b4f16586d584088d892a766a3501a6f093960
Evan <evanrock520l@ciencias.unam.mx>
2026-06-14 20:48:44 -0600

    feat: add BVH structure

---
+++ core/include/math/Geometry.hpp
@@ -0,0 +1,60 @@
+#pragma once
+
+#include "Types.hpp"
+#include "Eigen/Dense"
+
+namespace Tissu {
+
+inline Eigen::Vector3d closestPointOnTriangle(const Eigen::Vector3d& point,
+                                               const Eigen::Vector3d& a,
+                                               const Eigen::Vector3d& b,
+                                               const Eigen::Vector3d& c) {
+    Eigen::Vector3d ab = b - a;
+    Eigen::Vector3d ac = c - a;
+    Eigen::Vector3d ap = point - a;
+
+    double d1 = ab.dot(ap);
+    double d2 = ac.dot(ap);
+
+    if (d1 <= 0.0 && d2 <= 0.0)
+        return a;
+
+    Eigen::Vector3d bp = point - b;
+    double d3 = ab.dot(bp);
+    double d4 = ac.dot(bp);
+
+    if (d3 >= 0.0 && d4 <= d3)
+        return b;
+
+    Eigen::Vector3d cp = point - c;
+    double d5 = ab.dot(cp);
+    double d6 = ac.dot(cp);
+
+    if (d6 >= 0.0 && d5 <= d6)
+        return c;
+
+    double vc = d1 * d4 - d3 * d2;
+    if (vc <= 0.0 && d1 >= 0.0 && d3 <= 0.0) {
+        double t = d1 / (d1 - d3);
+        return a + t * ab;
+    }
+
+    double vb = d5 * d2 - d1 * d6;
+    if (vb <= 0.0 && d2 >= 0.0 && d6 <= 0.0) {
+        double t = d2 / (d2 - d6);
+        return a + t * ac;
+    }
+
+    double va = d3 * d6 - d5 * d4;
+    if (va <= 0.0 && (d4 - d3) >= 0.0 && (d5 - d6) >= 0.0) {
+        double t = (d4 - d3) / ((d4 - d3) + (d5 - d6));
+        return b + t * (c - b);
+    }
+
+    double denom = 1.0 / (va + vb + vc);
+    double v = vb * denom;
+    double w = vc * denom;
+    return a + v * ab + w * ac;
+}
+
+}
